1b9fb5171178d0dd6b408a68fee47de487113c47
havenwood <shannonskipper@gmail.com>
2013-03-04 19:26:35 -0800

    initial

---
+++ test/test_hashy.rb
@@ -0,0 +1,30 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'hashy'
+
+describe Hash do
+  before do
+    @hash = {one: 1, aim: true}
+  end
+  
+  describe 'Hash#map_pair' do
+    it 'modifies key and value' do
+      k_and_v_to_s = @hash.map_pair { |k, v| [k.to_s, v.to_s] }
+      assert_equal({'one' => '1', 'aim' => 'true'}, k_and_v_to_s)
+    end
+  end
+  
+  describe 'Hash#map_key' do
+    it 'modifies key only' do
+      k_to_s = @hash.map_key &:to_s
+      assert_equal({'one' => 1, 'aim' => true}, k_to_s)
+    end
+  end
+  
+  describe 'Hash#map_value' do
+    it 'modifies value only' do
+      v_to_s = @hash.map_value &:to_s
+      assert_equal({:one => '1', :aim => 'true'}, v_to_s)
+    end
+  end
+end
